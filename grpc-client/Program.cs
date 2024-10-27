d0bd6e9c4a0dc3c048e54f7272a48e129982a5d4
Reinaldo Junior <reinaldolejr@gmail.com>
2024-10-27 19:20:47 -0400

    wip: init  grpc client

---
+++ grpc-client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcService;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:7294");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Reinaldo" });
+            Console.WriteLine("Greeting: " + reply.Message);
+        }
+    }
+}
